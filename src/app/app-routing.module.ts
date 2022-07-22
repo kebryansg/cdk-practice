b38fb4f181deb1f87ed979f945eff161c535864d
shoniisra <jivillac@pichincha.com>
2022-07-22 11:31:31 -0500

    merge funcional

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,33 +4,30 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'table',
+    // redirectTo: 'table',
+    redirectTo: 'drag-and-drop',
     pathMatch: 'full'
   },
   {
     path: 'accordion',
     title: 'Accordion',
-    loadChildren: () => import('./pages/accordion/accordion.module').then(module => module.AccordionModule)
+    loadChildren: () => import('@accordion/accordion.module').then(module => module.AccordionModule)
   },
   {
     path: 'dialog',
     title: 'Dialog',
-    loadChildren: () => import('./pages/dialog/dialog.module').then(module => module.DialogModule)
+    loadChildren: () => import('@dialog/dialog.module').then(module => module.DialogModule)
   },
-  {
-    path: 'table',
-    title: 'Table',
-    loadChildren: () => import('./pages/table/table.module').then(module => module.TableModule)
-  }
-];
+  // {
+  //   path: 'table',
+  //   title: 'Table',
+  //   loadChildren: () => import('./pages/table/table.module').then(module => module.TableModule)
+  // },
   {
     path: 'drag-and-drop',
     loadChildren: () => import('@drag/drag-and-drop.module').then((m) => m.DragAndDropModule),
-  },
-  {
-    path: '',
-    component:AppComponent,
-  },
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
