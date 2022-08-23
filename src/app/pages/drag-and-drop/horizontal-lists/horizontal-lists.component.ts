b63de067fcd4d5af0dcfaa351b6c98f8cb8a37ec
shoniisra <jivillac@pichincha.com>
2022-08-23 16:16:21 -0500

    revision ejemplo

--- src/app/pages/drag-and-drop/horizontal-lists/horizontal-lists.component.ts
+++ src/app/pages/drag-and-drop/horizontal-lists/horizontal-lists.component.ts
@@ -1,30 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from "@angular/cdk/drag-drop";
 
 @Component({
-  selector: 'app-horizontal-lists',
-  templateUrl: './horizontal-lists.component.html',
-  styleUrls: ['./horizontal-lists.component.scss']
+    selector: 'app-horizontal-lists',
+    templateUrl: './horizontal-lists.component.html',
+    styleUrls: ['./horizontal-lists.component.scss']
 })
 export class HorizontalListsComponent implements OnInit {
-  todo = ['mariposa', 'vaca', 'ballena', 'tortuga'];
+    todo = ['mariposa', 'vaca', 'ballena', 'tortuga'];
 
-  done = ['perro', 'hormiga', 'pescado', 'ratón', 'araña'];
-  constructor() { }
+    done = ['perro', 'hormiga', 'pescado', 'ratón', 'araña'];
 
-  ngOnInit(): void {
-  }
-  drop(event: CdkDragDrop<string[]>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      transferArrayItem(
-          event.previousContainer.data,
-          event.container.data,
-          event.previousIndex,
-          event.currentIndex,
-      );
+    constructor() {
     }
-  }
+
+    ngOnInit(): void {
+    }
+
+    drop(event: CdkDragDrop<string[]>) {
+        if (event.previousContainer === event.container) {
+            moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+        } else {
+            transferArrayItem(
+                event.previousContainer.data,
+                event.container.data,
+                event.previousIndex,
+                event.currentIndex,
+            );
+        }
+    }
 
 }
